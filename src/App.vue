57d81f5d3b19667897a6580a4b2ae455994985e8
hako <lx.combox@gmail.com>
2023-09-13 19:26:22 +0800

    Use vanilla JS to manipulate Konva elements and finish transformer

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <script setup lang='ts'>
-import { Canvas } from 'konva/lib/Canvas';
-import { Image } from 'konva/lib/shapes/Image';
-import { Stage } from 'konva/lib/Stage';
-import { Transformer } from 'konva/lib/shapes/Transformer';
-import { setgroups } from 'process';
-import { arrayBuffer } from 'stream/consumers';
 import { ref, onMounted, onUnmounted } from 'vue'
+import Konva from 'konva';
+import { useTheme } from 'vuetify/lib/framework.mjs';
 
-const windowWidth = ref(window.innerWidth)
-const windowHeight = ref(window.innerHeight)
 
+let windowWidth: number = window.innerWidth
+let windowHeight: number = window.innerHeight / 2
+
+let CanvasResizeTimeOut: NodeJS.Timeout
+const UpdateKonvaCanvasSize = () => {
+  const toolbar:HTMLDivElement | null = document.querySelector('#toolbar');
+  if (toolbar == null) { return }
+  windowWidth =  window.innerWidth
+  windowHeight = window.innerHeight- toolbar.offsetHeight
+  // update Konva Canavs size
+  stage.value?.width(windowWidth)
+  stage.value?.height(windowHeight)
+}
 const handleWindowResize = () => {
-  windowWidth.value = window.innerWidth;
-  windowHeight.value = window.innerHeight
+  clearTimeout(CanvasResizeTimeOut)
+
+  CanvasResizeTimeOut = setTimeout(UpdateKonvaCanvasSize, 300)
 }
 
 onMounted(() => {
+  initKonva()
   window.addEventListener('resize', handleWindowResize)
 })
 
@@ -24,60 +33,44 @@ onUnmounted(() => {
 })
 
 
-const transformer = ref(null)
-const stage = ref(null)
+const transformerConfig = {
+  anchorStroke: 'white',
+  anchorFill: '#168e99',
+  anchorSize: 10,
+  anchorCornerRadius: 5,
+  borderStroke: '#168e99',
+  borderStrokeWidth: 2,
+  borderDash: [1, 0],
+  keepratio: true,
+}
+const transformer = new Konva.Transformer(transformerConfig)
 
-class RefImage {
-  name: string
-  image: HTMLImageElement
-  x: number
-  y: number
-  rotation: number
-  scaleX: number
-  scaleY: number
-  width: number
-  height: number
-  readonly draggable = true
-
+class RefImage extends Konva.Image {
   constructor(src: string) {
-    this.name = self.crypto.randomUUID()
-    this.image = new window.Image()
-    this.image.src = src
-    this.x = 0
-    this.y = 0
-    this.width = 200
-    this.height = 300
-    this.scaleX = 1.0
-    this.scaleY = 1.0
-    this.rotation = 0
+    const data = new window.Image()
+    data.src = src
+    super({
+      draggable: true,
+      image: data
+    })
+    this.id(self.crypto.randomUUID())
   }
 }
 
-class RefCavnas {
-  id: number
-  active: boolean
-  images: RefImage[]
-  constructor(id: number, active: boolean) {
-    this.id = id
-    this.images = []
-    this.active = active
+class RefLayers extends Konva.Layer {
+  index: number
+  constructor(id: number) {
+    super()
+    this.index = id
+    this.addName('refLayer')
   }
 }
 
 const active_canvas_id = ref(0)
-const canvases = Array.from(Array(6), (v, k) => (new RefCavnas(k, v == 0)))
-var selectedImgUUID: string = ''
-
-canvases[0].images.push(new RefImage('https://konvajs.github.io/assets/lion.png'))
-
-const configTransFormer = ref({
-  anchorStroke: 'red',
-  anchorFill: 'yellow',
-  anchorSize: 20,
-  borderStroke: 'green',
-  borderDash: [3, 3],
-  keepratio: true,
-})
+const layers = Array.from(Array(6), (v, k) => (new RefLayers(k)))
+const select_layer = new Konva.Layer()
+// konva elements
+const stage = ref<Konva.Stage | null>(null)
 
 const menu_items = [
   { title: 'Home', icon: 'mdi-home' },
@@ -86,49 +79,144 @@ const menu_items = [
 ]
 
 function switchCanvas(id: number) {
+  flushBackSelectedImgs()
+
   // filter out click-again case
   if (id == active_canvas_id.value) { return }
 
   // switch the active flag for button
   active_canvas_id.value = id
-  // TODO: unload the current canvas and load new canvas
-  //       maybe do some caching or memory cleaning?
-  // TODO: clean up the transformer
+  // unload the current canvas and load new canvas
+  loadCanvas()
+  // TODO:maybe do some caching or memory cleaning?
 }
 
-function handleStageMouseDown(e) {
-  console.log(e)
-  console.log("stage")
-  console.log(stage.value)
-  // if click on empty area - remove all transformers
-  if (e.target === stage.value) {
-    transformer.detach()
-    stage.value?.batchDraw()
+function flushBackSelectedImgs() {
+  const selected_imgs = select_layer.getChildren(
+    (node) => node.getClassName() === 'Image')
+  // return if no image selected
+  if (selected_imgs.length === 0) { return }
+  // move all selected images into current canvas
+  selected_imgs.forEach((node) => {
+    node.moveTo(layers[active_canvas_id.value])
+  })
+  // remove transformer from select layer
+  updateTransformer()
+}
+
+function handleStageMouseDown(e: any) {
+  const current_canvas = layers[active_canvas_id.value]
+  if (e.target == null || stage.value == null) { return }
+  else if (e.target === e.target.getStage()) {
+    // if click on empty area - remove all transformers
+    console.log('click on stage')
+    flushBackSelectedImgs()
+    // enable draggable for stage
+    stage.value.setDraggable(true)
+  } else if (e.target.getParent().className === 'Transformer') {
+    // if click on transformer - do nothing
+    console.log('click on transformer')
+    // do nothing
+  } else {
+    // click on image
+    console.log('click on image')
+
+    let clicked_uuid = e.target.id()
+    // check uuid is not null and stage exists
+    if (clicked_uuid == '') { return }
+
+    const refimg: RefImage = current_canvas.findOne('#' + clicked_uuid)
+    if (refimg == null) {
+      // the current image is already selected, abort
+      return
+    } else {
+      // disable draggable for stage
+      stage.value.setDraggable(false)
+      // move the image from current canvas to select layer
+      refimg.moveTo(select_layer)
+
+      updateTransformer()
+    }
+
   }
 }
 
-function handleImageMouseDown(e) {
-  console.log(stage.value)
-  stage.value?.find('Transformer').forEach((t) => t.destroy());
-  // create new transformer
-  transformer.value?.attachTo(e.target) // no idea what to do here ...
-  stage.value?.batchDraw()
+function updateTransformer() {
+  const selected_imgs = select_layer.getChildren(
+    (node) => node.getClassName() === 'Image')
+  if (selected_imgs.length === 0) {
+    // no image selected
+    transformer.remove()
+  } else {
+    // image selected 
+    transformer.nodes(selected_imgs)
+    select_layer.add(transformer)
+  }
 }
+
+
+/// konva start:
+function loadCanvas() {
+  // add the layer to the stage
+  stage.value?.removeChildren()
+  stage.value?.add(layers[active_canvas_id.value])
+  stage.value?.add(select_layer)
+
+  // TODO: maybe restore data from json
+  // instantialize the ref_images into canvas
+
+  // way to add blob konva.image
+  // const url = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
+  // fetch(url).then(res=>res.blob()).then((blob)=>{
+  //   const img = new Image()
+  //   img.src = URL.createObjectURL(blob)
+  //   img.onload = () => {
+  //     const pattern = new Konva.Image({
+  //       x: 100,
+  //       y: 100,
+  //       image: img,
+  //       width: 1000,
+  //       height: 1000,
+  //       draggable: true,
+  //     })
+  //     layer.add(pattern)
+  //     layer.batchDraw()
+  //   }
+  // })
+}
+
+function initKonva() {
+  stage.value = new Konva.Stage({
+    container: 'konva_container',   // id of container <div>
+    width: windowWidth,
+    height: windowHeight
+  })
+  UpdateKonvaCanvasSize()
+  stage.value?.on('mousedown', handleStageMouseDown)
+
+  const img0 = new RefImage('https://konvajs.github.io/assets/lion.png')
+  const img1 = new RefImage('https://konvajs.github.io/assets/lion.png')
+  const img2 = new RefImage('https://konvajs.github.io/assets/lion.png')
+  layers[0].add(img0)
+  layers[0].add(img1)
+  layers[2].add(img2)
+
+  loadCanvas()
+}
+
 
 </script>
 
 
 
 <template>
-  <header>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>
-
-    <v-toolbar absolute density='compact'>
-      <v-btn variant='flat' color='primary'>
+  <v-layout class='rounded rounded-md'>
+    <v-app-bar fluid flat density='compact' color='#efefef' id='toolbar'>
+      <v-btn variant='flat' color='primary' class='ml-3'>
         Menu
         <v-menu activator='parent'>
           <v-list nav density='compact'>
-            <v-list-item v-for='(item, index) in menu_items' :key='index' :value='index'>
+            <v-list-item v-for='item in menu_items'>
               <v-list-item-title> {{ item.title }} </v-list-item-title>
             </v-list-item>
           </v-list>
@@ -136,42 +224,39 @@ function handleImageMouseDown(e) {
       </v-btn>
 
       <v-btn-group rounded='0' class='ml-3' density='compact'>
-        <v-btn :variant="active_canvas_id === item.id ? 'tonal' : 'text'" class='navbtn px-1 ma-1'
-          v-for='(item, index) in canvases' :key='index' @click='switchCanvas(item.id)'>
-          {{ item.id }}
+        <v-btn :variant="active_canvas_id === i ? 'tonal' : 'text'" class='navbtn px-1 ma-1' v-for='i in Array(6).keys()'
+          @click='switchCanvas(i)'>
+          {{ i }}
         </v-btn>
       </v-btn-group>
+    </v-app-bar>
+    <v-main fluid id="konva_container">
+    </v-main>
 
-      <v-toolbar-items class='ml-6'>
-        <ul>
-          <li>Screen Width: {{ windowWidth }}</li>
-          <li>Screen Height: {{ windowHeight }}</li>
-        </ul>
-      </v-toolbar-items>
-    </v-toolbar>
-  </header>
+  </v-layout>
 
   <!-- TODO: 两个layer，一个是编辑中的对象（可能多个），另一个是剩余的 -->
   <!-- TODO: v-stage最终也会作为动态对象保存，保存一个-->
-  <v-stage :width='windowWidth' :height='windowHeight' :ref="stage" @mousedown="handleStageMouseDown"
-    @touchStart="handleStageMouseDown">
-    <v-layer>
-      <v-image v-for='ref_image in canvases[active_canvas_id].images' :config='ref_image'
-        @mousedown="handleImageMouseDown" @touchStart="handleImageMouseDown" />
-      <!-- v-transformer不靠谱，考虑用vanilla json做一个 -->
-      <v-transformer :config='configTransFormer' :ref='transformer' />
+  <!-- <v-stage ref='stage' :width='windowWidth' :height='windowHeight' @mousedown="handleStageMouseDown"
+    @touchStart='handleStageMouseDown'>
+    <v-layer ref='layer_select'>
+      <v-image v-for='selected_img in selected_imgs' :config='selected_img' />
     </v-layer>
-  </v-stage>
+    <v-layer>
+      <v-image v-for='ref_image in canvases[active_canvas_id].images' :config='ref_image' />
+    </v-layer>
+  </v-stage> -->
 </template>
 
 <style scoped>
 header {
+  position:absolute;
   line-height: 1.5;
-  max-height: 100vh;
+  max-height: 100vh
 }
 
 .v-toolbar {
-  z-index: 1000;
+  z-index: 100
 }
 
 .navbtn {
